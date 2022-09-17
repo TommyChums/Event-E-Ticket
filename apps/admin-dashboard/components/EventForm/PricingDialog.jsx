3d37cae80647900ed6fab4a17fa1b45420c8f35b
ricardofelix2@yahoo.com <ricardofelix2@yahoo.com>
2022-09-17 16:10:35 -0400

    Update the form for managing event | Added pricing dialog box

---
+++ apps/admin-dashboard/components/EventForm/PricingDialog.jsx
@@ -0,0 +1,226 @@
+import React, { useCallback, useEffect, useState } from 'react'
+import { useController, useForm } from 'react-hook-form';
+import forEach from 'lodash/forEach';
+import isEmpty from 'lodash/isEmpty';
+import isNumber from 'lodash/isNumber';
+import map from 'lodash/map';
+import snakeCase from 'lodash/snakeCase';
+import startCase from 'lodash/startCase';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import DialogTitle from '@mui/material/DialogTitle';
+import Dialog from '@mui/material/Dialog';
+import AddIcon from '@mui/icons-material/Add';
+import DeleteIcon from '@mui/icons-material/Delete';
+import TextField from '@mui/material/TextField';
+import Typography from '@mui/material/Typography';
+import Stack from '@mui/material/Stack';
+
+function ControlledNumberInput({ control, name, defaultValue, ...props }) {
+  const { field, fieldState: { error } } = useController({
+    control,
+    name,
+    defaultValue,
+    //TODO: See why this isn't working
+    rules: {
+      required: 'Required',
+      pattern: {
+        value: /^[0-9]+$/,
+        message: 'Whole Number',
+      },
+    },
+  });
+
+  return (
+    <TextField
+      {...props}
+      {...field}
+      onChange={({ target }) => {
+        const { value } = target;
+
+        if (isNumber(value)) {
+          field.onChange(+value);
+        } else {
+          field.onChange(value);
+        }
+      }}
+      error={!!error}
+      helperText={error?.message}
+    />
+  );
+};
+
+export default function PricingDialog(props) {
+  const { onClose, open, value, onChange } = props;
+
+  const [ newAgeRange, setNewAgeRange ] = useState('');
+
+  const [ pricesByAge, setPricesByAge ] = useState({});
+  const [ ageMapping, setAgeMapping ] = useState({});
+
+  const [ ageRangesToDelete, setAgeRangesToDelete ] = useState({});
+
+  const setDefaultsFromValue = useCallback((val) => {
+    if (isEmpty(val)) return;
+
+    setPricesByAge(val.price_by_age);
+    setAgeMapping(val.age_mapping);
+  }, [ setPricesByAge, setAgeMapping ]);
+
+  const {
+    control,
+    handleSubmit,
+    reset,
+    formState: {
+      isValid,
+    },
+  } = useForm({
+    defaultValues: {
+      ...value || {},
+    },
+  });
+
+  useEffect(() => {
+   setDefaultsFromValue(value);
+   setAgeRangesToDelete({});
+  }, [ setDefaultsFromValue, value ]);
+
+  const onSubmit = (data, e) => {
+    const newData = { ...data };
+
+    forEach(ageRangesToDelete, (_, ageLabel) => {
+      delete newData?.age_mapping[ageLabel];
+      delete newData?.price_by_age[ageLabel];
+    });
+
+    setAgeRangesToDelete({});
+    onChange(data);
+    onClose();
+  };
+
+  const onReset = () => {
+    reset(value || {});
+    setDefaultsFromValue(value);
+    setAgeRangesToDelete({});
+    onClose();
+  };
+
+  const handleAddNewAgeRange = () => {
+    if (!newAgeRange) return;
+
+    const newCasedAgeRange = snakeCase(newAgeRange);
+    
+    setPricesByAge({
+      [newCasedAgeRange]: 0,
+      ...pricesByAge,
+    });
+    setAgeMapping({
+      [newCasedAgeRange]: {
+        from: 0,
+        to: 9999,
+      },
+      ...ageMapping,
+    });
+    
+    setNewAgeRange('');
+
+    if (ageRangesToDelete[newCasedAgeRange]) {
+      const temp = { ...ageRangesToDelete };
+
+      delete temp[newCasedAgeRange];
+
+      setAgeRangesToDelete(temp);
+    }
+  };
+
+  const handleDeleteAgeRange = (ageLabel) => {
+    const newAgeMapping = {...ageMapping};
+    const newPricesByAge = {...pricesByAge};
+
+    delete newAgeMapping[ageLabel];
+    delete newPricesByAge[ageLabel];
+
+    setAgeMapping(newAgeMapping);
+    setPricesByAge(newPricesByAge);
+
+    setAgeRangesToDelete({
+      ...ageRangesToDelete,
+      [ageLabel]: true,
+    });
+  };
+
+  return (
+    <Dialog onClose={onReset} open={open} fullWidth>
+      <DialogTitle>Define the pricing structure</DialogTitle>
+      <Stack mx={2} mb={4} spacing={1}>
+        {map(pricesByAge, (price, ageLabel) => (
+          <Stack key={ageLabel} spacing={1}>
+            <Typography
+              sx={{ mt: 0.5, ml: 0 }}
+              color="text.secondary"
+              display="block"
+              variant="caption"
+            >
+              {startCase(ageLabel)}
+            </Typography>
+            <Stack direction="row" spacing={1}>
+              <ControlledNumberInput
+                control={control}
+                name={`age_mapping.${ageLabel}.from`}
+                defaultValue={ageMapping[ageLabel]?.from}
+                label="Age From:"
+              />
+              <ControlledNumberInput
+                control={control}
+                name={`age_mapping.${ageLabel}.to`}
+                defaultValue={ageMapping[ageLabel]?.to}
+                label="Age To:"
+              />
+              <ControlledNumberInput
+                control={control}
+                name={`price_by_age.${ageLabel}`}
+                defaultValue={price}
+                label="Price:"
+              />
+              <IconButton onClick={() => handleDeleteAgeRange(ageLabel)}>
+                <DeleteIcon />
+              </IconButton>
+            </Stack>
+          </Stack>
+        ))}
+      </Stack>
+      <Stack mx={2} direction="row" spacing={1}>
+        <TextField
+          label="New Age Range"
+          variant="outlined"
+          onChange={({ target: { value } }) => setNewAgeRange(value)}
+          fullWidth
+        />
+        <IconButton
+          onClick={() => handleAddNewAgeRange()}
+        >
+          <AddIcon />
+        </IconButton>
+      </Stack>
+      <Stack direction="row" sx={{ mx: 2, mr: 8, my: 2 }} spacing={1}>
+        <Button
+          disabled={!isValid}
+          variant="contained"
+          type="button"
+          onClick={handleSubmit(onSubmit)}
+          fullWidth
+        >
+          Save
+        </Button>
+        <Button
+          variant="outlined"
+          type="button"
+          onClick={onReset}
+          fullWidth
+        >
+          Cancel
+        </Button>
+      </Stack>
+    </Dialog>
+  );
+}
--- apps/admin-dashboard/components/EventForm/index.jsx
+++ apps/admin-dashboard/components/EventForm/index.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import { Controller, useController, useForm } from 'react-hook-form';
+import isEqual from 'lodash/isEqual';
 import Stack from '@mui/material/Stack';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
@@ -9,6 +10,7 @@ import { AdapterMoment } from '@mui/x-date-pickers/AdapterMoment';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { DateTimePicker } from '@mui/x-date-pickers/DateTimePicker';
 import { usePlacesWidget } from "react-google-autocomplete";
+import PricingDialog from './PricingDialog';
 
 function ControlledLocation({ control, name, defaultValue, rules, ...props }) {
   const { field: { onChange, value } } = useController({ control, name, defaultValue, rules });
@@ -40,21 +42,42 @@ function ControlledLocation({ control, name, defaultValue, rules, ...props }) {
 };
 
 export default function EventForm({ event = {} }) {
+  const [ pricingDialogOpen, setPricingDialogOpen ] = useState(false);
 
   const {
     control,
     handleSubmit,
     reset,
+    unregister,
+    formState,
   } = useForm({
     mode: 'onBlur',
-    defaultValues: event,
+    defaultValues: { ...event },
   });
+
+  const { isValid, isDirty, isSubmitting } = formState;
+
+  const submitDisabled = !isValid || isSubmitting;
+
+  const onSubmit = (data) => {
+    // TODO: Update to only save if changes made
+    // TODO: Update to actually update event in db
+    // if (isEqual(event, data)) {
+    //   console.log('Same event, no update');
+    // } else {
+      console.log(data);
+    // }
+  };
+
+  const onReset = () => {
+    reset(event); 
+  };
 
   return (
     <Container maxWidth="md" sx={{ marginBottom: '2rem' }}>
       <form
-        onSubmit={handleSubmit(console.log)}
-        onReset={() => reset(event, { keepDefaultValues: true })}
+        onSubmit={handleSubmit(onSubmit)}
+        onReset={onReset}
       >
         <Stack direction="column" spacing={2}>
           <Typography variant="h6">
@@ -151,13 +174,38 @@ export default function EventForm({ event = {} }) {
           <ControlledLocation
             control={control}
             name="venue"
+            placeholder="Search Google Maps"
             defaultValue={event.venue}
+            InputLabelProps={{ shrink: true }}
           />
           <Typography variant="h6">
             Ticket Information
           </Typography>
+          <Controller
+            control={control}
+            name="payment_config"
+            defaultValue={{}}
+            render={({ field }) => (
+              <PricingDialog
+                {...field}
+                control={control}
+                open={pricingDialogOpen}
+                unregister={unregister}
+                onClose={() => setPricingDialogOpen(false)}
+              />
+            )}
+          />
+          <Button
+            fullWidth
+            variant="contained"
+            type="button"
+            onClick={() => setPricingDialogOpen(true)}
+          >
+            Configure Pricing
+          </Button>
+
           <Stack direction="row" spacing={1} sx={{ justifyContent: 'space-between' }}>
-            <Button fullWidth variant="contained" type="submit">Submit</Button>
+            <Button disabled={submitDisabled} fullWidth variant="contained" type="submit">Submit</Button>
             <Button fullWidth variant="outlined" type="reset">Reset</Button>
           </Stack>
         </Stack>
