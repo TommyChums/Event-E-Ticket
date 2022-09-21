595e287c9e90abdf2a3ef547c77f9204662418df
ricardofelix2@yahoo.com <ricardofelix2@yahoo.com>
2022-09-21 13:12:25 -0400

    Updated registration email template

--- apps/registration-form/lib/supabase.js
+++ apps/registration-form/lib/supabase.js
@@ -4,3 +4,8 @@ export default createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL,
   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
 );
+
+export const auththenticatedSupabase = createClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL,
+  process.env.SUPABASE_SERVICE_KEY,
+);
--- apps/registration-form/pages/api/registration-email.js
+++ apps/registration-form/pages/api/registration-email.js
@@ -1,12 +1,49 @@
 import nodemailer from 'nodemailer';
-import pug from 'pug';
+import { decode } from 'base64-arraybuffer';
+import sharp from 'sharp';
 import Handlebars from 'handlebars';
 import path from 'path';
 import findKey from 'lodash/findKey';
 import moment from 'moment';
 import fs from 'fs';
 
-import supabase from "../../lib/supabase";
+import supabase, { auththenticatedSupabase } from "../../lib/supabase";
+
+function svgImgUrl(svgName, primaryColour) {
+  const { publicURL } = supabase.storage.from('church-assets').getPublicUrl(`email-images/${primaryColour}/${svgName}.png`);
+
+  return publicURL;
+};
+
+async function getImgUrl(svgName, primaryColour) {
+  const colourFolder = primaryColour.replace('#', '');
+
+  const { data: colourSvgFile } = await supabase.storage.from('church-assets').list(`email-images/${colourFolder}`, {
+    limit: 1,
+    offset: 0,
+    sortBy: { column: 'name', order: 'asc' },
+    search: `${svgName}.png`,
+  });
+
+  if (colourSvgFile.length) {
+    return svgImgUrl(svgName, colourFolder);
+  }
+
+  const svgData = fs.readFileSync(path.join(process.cwd(), `assets/svg-images/${svgName}.svg`)).toString();
+
+  const svgString = Handlebars.compile(svgData)({ primaryColour });
+
+  const svgImgArrayBuffer = await sharp(
+    Buffer.from(svgString)
+  ).ensureAlpha(0)
+   .png()
+   .toBuffer()
+   .then((img) => decode(img.toString('base64')));
+
+  await auththenticatedSupabase.storage.from('church-assets').upload(`email-images/${colourFolder}/${svgName}.png`, svgImgArrayBuffer, { contentType: 'image/png', upsert: true });
+
+  return svgImgUrl(svgName, colourFolder);
+}
 
 export default async function handler(req, res) {
   if (req.method === 'GET') {
@@ -36,9 +73,19 @@ export default async function handler(req, res) {
 
     const userPrice = `$${priceByAge[userAgeMapping] || 0}`;
 
+    // TODO: Get from event itself
+    const primaryColour = '#ecc721';
+
     const { publicURL: logoUrl } = supabase.storage.from(event.logo?.bucket).getPublicUrl(event.logo?.key);
     
     const { publicURL: rlcLogo } = supabase.storage.from('church-assets').getPublicUrl('logo.png');
+
+    const [ calendarIcon, locationIcon, facebookIcon, youtubeIcon ] = await Promise.all([
+      getImgUrl('calendar', primaryColour),
+      getImgUrl('location', primaryColour),
+      getImgUrl('facebook', primaryColour),
+      getImgUrl('youtube', primaryColour),
+    ]);
 
     const registrationPugPath = fs.readFileSync(path.join(process.cwd(), 'assets/email-templates/registration.html')).toString();
  
@@ -55,7 +102,14 @@ export default async function handler(req, res) {
       replySubject: `Event Registration: ${event.name}`,
       logoUrl,
       rlcLogo,
+      primaryColour,
+      calendarIcon,
+      locationIcon,
+      facebookIcon,
+      youtubeIcon,
     });
+
+    fs.writeFileSync('email.html', ticketHtml);
 
     const transporter = nodemailer.createTransport({
       service: 'gmail',
