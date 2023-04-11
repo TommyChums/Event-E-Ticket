8cd411a1b6cb82f18801654110db26d99e97f82d
ricardofelix2@yahoo.com <ricardofelix2@yahoo.com>
2023-04-11 13:38:56 -0400

    Separate past events from current events

--- apps/admin-dashboard/components/TabPanel/index.jsx
+++ apps/admin-dashboard/components/TabPanel/index.jsx
@@ -1,7 +1,7 @@
 import PropTypes from 'prop-types';
 
 export default function TabPanel(props) {
-  const { children, value, index, ...other } = props;
+  const { children, value, index, fullWidth, ...other } = props;
 
   return (
     <div
@@ -9,6 +9,10 @@ export default function TabPanel(props) {
       hidden={value !== index}
       id={`simple-tabpanel-${index}`}
       role="tabpanel"
+      style={{
+        ...other.style || {},
+        ...fullWidth ? { width: '100%' } : {},
+      }}
       {...other}
     >
       {value === index &&
--- apps/admin-dashboard/pages/events/index.jsx
+++ apps/admin-dashboard/pages/events/index.jsx
@@ -1,8 +1,14 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Head from 'next/head';
 import { useRouter } from 'next/router';
+import moment from 'moment';
+import forEach from 'lodash/forEach';
 import isEmpty from 'lodash/isEmpty';
 import map from 'lodash/map';
+import Box from '@mui/material/Box';
+import Tab from '@mui/material/Tab';
+import Tabs from '@mui/material/Tabs';
+import TabPanel from '../../components/TabPanel';
 import Typography from '@mui/material/Typography';
 import Stack from '@mui/material/Stack';
 import Card from '@mui/material/Card';
@@ -21,9 +27,32 @@ export const getServerSideProps = protectedRoute();
 
 export default function EventsHome() {
   const { events, loading } = useEvents();
+  const [ pastEvents, setPastEvents ] = useState([]);
+  const [ currentEvents, setCurrentEvents ] = useState([]);
   const [ isRouting, setRouting ] = useState(false);
   const isSmallScreen = useMediaQuery('(max-width:900px)');
   const router = useRouter();
+  
+  const [ value, setValue ] = useState(1);
+  const handleChange = (_, newValue) => {
+    setValue(newValue);
+  };
+
+  useEffect(() => {
+    const pEs = [];
+    const cEs = [];
+
+    forEach(events, (event) => {
+      if (moment(event.end_date).isBefore(moment())) {
+        pEs.push(event);
+      } else {
+        cEs.push(event);
+      }
+    });
+
+    setPastEvents(pEs);
+    setCurrentEvents(cEs);
+  }, [ events ]);
 
   const pushToPage = (pathName) => {
     setRouting(true);
@@ -51,6 +80,9 @@ export default function EventsHome() {
           justifyContent="center"
           maxWidth="lg"
           spacing={2}
+          style={{
+            width: '100%'
+          }}
         >
           <Stack
             alignItems="center"
@@ -72,46 +104,107 @@ export default function EventsHome() {
               }
             </Typography>
           </Stack>
-          <Grid container justifyContent={isSmallScreen ? 'center' : 'start'} spacing={2} width="100%">
-            {
-              map(events, (event) => {
-                const eventPagePath = `/events/${event.uuid}`;
+          {
+            !isEmpty(events) && (
+              <>
+                <Box sx={{ width: '100%' }}>
+                  <Tabs
+                    aria-label="Events Menu"
+                    onChange={handleChange}
+                    value={value}
+                    variant="fullWidth"
+                  >
+                    <Tab label="Past Events" value={0} />
+                    <Tab label="Current Events" value={1} />
+                  </Tabs>
+                </Box>
+                <TabPanel fullWidth index={0} value={value}>
+                  <Grid container justifyContent={isSmallScreen ? 'center' : 'start'} spacing={2} width="100%">
+                    {
+                      map(pastEvents, (event) => {
+                        const eventPagePath = `/events/${event.uuid}`;
 
-                return (
-                  <Grid item key={event.uuid} style={{ padding: '1rem 10px' }}>
-                    <Card sx={{ width: 280, height: '100%' }}>
-                      <CardActionArea
-                        onClick={() => pushToPage(eventPagePath)}
-                        sx={{
-                          display: 'flex',
-                          flexDirection: 'column',
-                          justifyContent: 'start',
-                          height: '100%'
-                        }}
-                      >
-                        <CardMedia
-                          alt={event.name}
-                          component="img"
-                          height="140"
-                          image={event.logo}
-                          sx={{ width: '140px', height: '140px', alignSelf: 'center' }}
-                          width="140"
-                        />
-                        <CardContent>
-                          <Typography component="div" gutterBottom textAlign="center" variant="h5">
-                            {event.name}
-                          </Typography>
-                          <Typography color="text.secondary" variant="body2">
-                            {event.description}
-                          </Typography>
-                        </CardContent>
-                      </CardActionArea>
-                    </Card>
+                        return (
+                          <Grid item key={event.uuid} style={{ padding: '1rem 10px' }}>
+                            <Card sx={{ width: 280, height: '100%' }}>
+                              <CardActionArea
+                                onClick={() => pushToPage(eventPagePath)}
+                                sx={{
+                                  display: 'flex',
+                                  flexDirection: 'column',
+                                  justifyContent: 'start',
+                                  height: '100%'
+                                }}
+                              >
+                                <CardMedia
+                                  alt={event.name}
+                                  component="img"
+                                  height="140"
+                                  image={event.logo}
+                                  sx={{ width: '140px', height: '140px', alignSelf: 'center' }}
+                                  width="140"
+                                />
+                                <CardContent>
+                                  <Typography component="div" gutterBottom textAlign="center" variant="h5">
+                                    {event.name}
+                                  </Typography>
+                                  <Typography color="text.secondary" variant="body2">
+                                    {event.description}
+                                  </Typography>
+                                </CardContent>
+                              </CardActionArea>
+                            </Card>
+                          </Grid>
+                        );
+                      })
+                    }
                   </Grid>
-                );
-              })
-            }
-          </Grid>
+                </TabPanel>
+                <TabPanel fullWidth index={1} value={value}>
+                  <Grid container justifyContent={isSmallScreen ? 'center' : 'start'} spacing={2} width="100%">
+                    {
+                      map(currentEvents, (event) => {
+                        const eventPagePath = `/events/${event.uuid}`;
+
+                        return (
+                          <Grid item key={event.uuid} style={{ padding: '1rem 10px' }}>
+                            <Card sx={{ width: 280, height: '100%' }}>
+                              <CardActionArea
+                                onClick={() => pushToPage(eventPagePath)}
+                                sx={{
+                                  display: 'flex',
+                                  flexDirection: 'column',
+                                  justifyContent: 'start',
+                                  height: '100%'
+                                }}
+                              >
+                                <CardMedia
+                                  alt={event.name}
+                                  component="img"
+                                  height="140"
+                                  image={event.logo}
+                                  sx={{ width: '140px', height: '140px', alignSelf: 'center' }}
+                                  width="140"
+                                />
+                                <CardContent>
+                                  <Typography component="div" gutterBottom textAlign="center" variant="h5">
+                                    {event.name}
+                                  </Typography>
+                                  <Typography color="text.secondary" variant="body2">
+                                    {event.description}
+                                  </Typography>
+                                </CardContent>
+                              </CardActionArea>
+                            </Card>
+                          </Grid>
+                        );
+                      })
+                    }
+                  </Grid>
+                </TabPanel>
+              </>
+            )
+          }
         </Stack>
       </Stack>
     </>
